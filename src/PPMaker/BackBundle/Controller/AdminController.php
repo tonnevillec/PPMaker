bc10c032a138cf0b8ba58166f2d0fc9b364e2278
c.tonneville <cyril.tonneville@fiducial.net>
2016-06-17 15:57:29 +0200

    Creation des bundle Front et Back

---
+++ src/PPMaker/BackBundle/Controller/AdminController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace PPMaker\BackBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+//use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
+
+class AdminController extends Controller
+{
+    /**
+     * @Route("/", name="back_home")
+     * @Template("PPMakerBackBundle:Admin:index.html.twig")
+     */
+    public function indexAction()
+    {
+        //$authCheck = $this->get('security.authorization_checker');
+        //if ($authCheck->isGranted('ROLE_ADMIN')) {
+            //return $this->redirect($this->generateUrl('back_admin'));
+        //}
+        return $this->redirect($this->generateUrl('back_login'));
+    }
+
+    /**
+     * @Route("/home", name="back_admin")
+     * @Template("PPMakerBackBundle:Admin:admin.html.twig")
+     */
+    public function indexAdminAction()
+    {
+        // A ajouter avant pubf
+        //* @Security("has_role('ROLE_ADMIN')")
+        return ;
+    }
+
+    /**
+     * @Route("/login", name="back_login")
+     * @Template("PPMakerBackBundle:Admin:login.html.twig")
+     */
+    public function loginAdminAction()
+    {
+        return ;
+    }
+}
---
+++ src/PPMaker/BackBundle/PPMakerBackBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace PPMaker\BackBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class PPMakerBackBundle extends Bundle
+{
+}
